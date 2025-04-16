f8498a2b7112e507cab10b2e6ae3747d193c61d2
loiterlion <brucewang7777@gmail.com>
2025-04-16 13:49:59 +0800

    Chapter 5 Challenge 1 completed

--- Chapter 5/Chapter5_Challenge1/Chapter5 Challenge1/Chapter5 Challenge1/ViewController.swift
+++ Chapter 5/Chapter5_Challenge1/Chapter5 Challenge1/Chapter5 Challenge1/ViewController.swift
@@ -5,6 +5,7 @@
 //  Created by Bruce on 2025/4/15.
 //
 
+import RxSwift
 import UIKit
 
 public func example(of description: String,
@@ -17,7 +18,76 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        test()
+    }
+        
+    func test() {
+        example(of: "Challenge 1") {
+          let disposeBag = DisposeBag()
+          
+          let contacts = [
+            "603-555-1212": "Florent",
+            "212-555-1212": "Shai",
+            "408-555-1212": "Marin",
+            "617-555-1212": "Scott"
+          ]
+          
+          func phoneNumber(from inputs: [Int]) -> String {
+            var phone = inputs.map(String.init).joined()
+            
+            phone.insert("-", at: phone.index(
+              phone.startIndex,
+              offsetBy: 3)
+            )
+            
+            phone.insert("-", at: phone.index(
+              phone.startIndex,
+              offsetBy: 7)
+            )
+            
+            return phone
+          }
+          
+          let input = PublishSubject<Int>()
+          
+          // Add your code here
+            input
+                .skipWhile { String.init($0).starts(with: "0") }
+                .filter { $0 < 10 }
+                .take(10)
+                .toArray()
+                .subscribe(onSuccess: { numbers in
+                    let phone = phoneNumber(from: numbers)
+                    
+                    if let contact = contacts[phone] {
+                      print("Dialing \(contact) (\(phone))...")
+                    } else {
+                      print("Contact not found")
+                    }
+                }, onError: { error in
+                    print(error)
+                })
+                .disposed(by: disposeBag)
+          
+          
+          input.onNext(0)
+          input.onNext(603)
+          
+          input.onNext(2)
+          input.onNext(1)
+          
+          // Confirm that 7 results in "Contact not found",
+          // and then change to 2 and confirm that Shai is found
+          input.onNext(2)
+          
+          "5551212".forEach {
+            if let number = (Int("\($0)")) {
+              input.onNext(number)
+            }
+          }
+          
+          input.onNext(9)
+        }
     }
 }
 
